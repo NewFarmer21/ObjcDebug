f45f06d4efc7083541c3d74ac2146cb96a907744
dangguo <dangguo001@ke.com>
2019-03-19 12:40:55 +0800

    添加可调试的objc项目

---
+++ objc-750.1_debug/ObjcDebuyTarget/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  ObjcDebuyTarget
+//
+//  Created by 党国 on 2019/3/19.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        NSObject *obj = [[NSObject alloc] init];
+        obj = nil;
+        NSLog(@"%@", obj);
+    }
+    return 0;
+}
